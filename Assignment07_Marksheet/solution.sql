008f1c296ac4e59c370eca9ed27c0d8fe2acb2c9
1U25CA079-VSakthiganesh <thalapathysakthi51@gmail.com>
2026-07-30 09:39:57 +0530

    Update solution.sql

--- Assignment07_Marksheet/solution.sql
+++ Assignment07_Marksheet/solution.sql
@@ -8,3 +8,19 @@ USE CollegeDB;
 -- Display students with Marks > 80
 
 -- Sort by Marks DESC
+CREATE TABLE Marksheet (
+  RollNo INT PRIMARY KEY,
+  Name VARCHAR (50)
+  Department VARCHAR(20)
+  Marks INT
+ 
+  INSERT INTO Marksheet VALUES (1,'Arun','CSE',85);
+  INSERT INTO Marksheet VALUES (2,'Divya','IT',78);
+  INSERT INTO Marksheet VALUES (3,'Karthik','CSE',92);
+  INSERT INTO Marksheet VALUES (4,'Nisha','ECE',67);
+  INSERT INTO Marksheet VALUES (5,'Rahul','it',88);
+ 
+  SELECT *
+  FROM Marksheet
+  WHERE Marks>80
+  ORDER BY Marks DESC
